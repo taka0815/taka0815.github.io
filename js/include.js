df54761d649fb86fc708ca526acfc15cea8f1232
osada takafumi <taka0815basket@icloud.com>
2026-02-12 01:10:47 +0900

    file name change

--- js/include.js
+++ js/include.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
 
     // header読み込み
-    fetch("./_parts/header.html")
+    fetch("./parts/header.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("header").innerHTML = data;
         });
 
     // footer読み込み
-    fetch("./_parts/footer.html")
+    fetch("./parts/footer.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("footer").innerHTML = data;
